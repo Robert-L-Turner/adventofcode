9f6b82705764fd33e7a3a18be6700491218422c8
robertlturner <robert.lucas.turner@gmail.com>
2021-07-05 22:27:55 +0800

    Setup vim for C linting and fixing using ALE
    Identified error on logical && vs bitwise &

--- 2020/main2.c
+++ 2020/main2.c
@@ -3,69 +3,71 @@
 #include <string.h>
 
 struct Rule {
-	int num1;
-	int num2;
-	char letter;
-	char *password;
+  int num1;
+  int num2;
+  char letter;
+  char *password;
 };
 
 struct Rule load_rule(char *str) {
-	struct Rule tmprule;
+  struct Rule tmprule;
 
-	tmprule.num1 = atoi(strtok(str, "- :"));
-	tmprule.num2 = atoi(strtok(NULL, "- :"));
-	tmprule.letter = *strtok(NULL, "- :");
-	tmprule.password = strtok(NULL, "- :");
+  tmprule.num1 = atoi(strtok(str, "- :"));
+  tmprule.num2 = atoi(strtok(NULL, "- :"));
+  tmprule.letter = *strtok(NULL, "- :");
+  tmprule.password = strtok(NULL, "- :");
 
-	return tmprule;
+  return tmprule;
 }
 
 int part1(struct Rule tmprule) {
-	int count = 0;
-	for (int i=0; i<strlen(tmprule.password); i++) {
-		if (tmprule.letter == tmprule.password[i] ) {
-			count++;
-		}	
-	}
-	if ( (count >= tmprule.num1) && (count <= tmprule.num2)) {
-		return 1;
-	}
-	else { return 0; }
+  int count = 0;
+  for (int i = 0; i < (int)strlen(tmprule.password); i++) {
+    if (tmprule.letter == tmprule.password[i]) {
+      count++;
+    }
+  }
+  if ((count >= tmprule.num1) && (count <= tmprule.num2)) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
 int part2(struct Rule tmprule) {
-	if ((tmprule.password[tmprule.num1-1] == tmprule.letter) ^ (tmprule.password[tmprule.num2-1] == tmprule.letter)) {
-		return 1;
-	}
-	else { return 0;}
+  if ((tmprule.password[tmprule.num1 - 1] == tmprule.letter) ^
+      (tmprule.password[tmprule.num2 - 1] == tmprule.letter)) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
-int main(int argc, char * argv[]) {
-	
-	if ( argc != 2){
-		printf("Usage : %s [inputfile]\n", argv[0]);
-		exit(-1);
-	}
-	
-	FILE *fp;
-	if ((fp = fopen(argv[1], "r")) == NULL){
-		puts("Error reading file");
-		exit(-1);
-	}
-	
-	char buf[200];
-	int part1_count = 0, part2_count = 0;
+int main(int argc, char *argv[]) {
 
-	while ( fgets(buf, sizeof buf, fp) != NULL) {
-		struct Rule tmprule = load_rule(buf);
-		part1_count += part1(tmprule);		
-		part2_count += part2(tmprule);
-	}
-	
-	printf("Part1 Pass Count: %d\n", part1_count);	
-	printf("Part2 Pass Count: %d\n", part2_count);	
-	
-	
-	fclose(fp);
-	return 0;
+  if (argc != 2) {
+    printf("Usage : %s [inputfile]\n", argv[0]);
+    exit(-1);
+  }
+
+  FILE *fp;
+  if ((fp = fopen(argv[1], "r")) == NULL) {
+    puts("Error reading file");
+    exit(-1);
+  }
+
+  char buf[200];
+  int part1_count = 0, part2_count = 0;
+
+  while (fgets(buf, sizeof buf, fp) != NULL) {
+    struct Rule tmprule = load_rule(buf);
+    part1_count += part1(tmprule);
+    part2_count += part2(tmprule);
+  }
+
+  printf("Part1 Pass Count: %d\n", part1_count);
+  printf("Part2 Pass Count: %d\n", part2_count);
+
+  fclose(fp);
+  return 0;
 }
--- 2020/main3.c
+++ 2020/main3.c
@@ -2,37 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-int test (FILE *fp, int x, int right, int down, int divisor) {
-	int trees = 0, row = 0;
-	char buf[200];
-	while ( fgets(buf, sizeof(buf), fp) != NULL) {
-		if (row % down == 0) {
-			if (buf[x] == '#') {trees++;}
-			x = (x+right) % divisor;
-		}
-	        row++;
-	}
-	fseek(fp, 0, SEEK_SET);
-	return trees;
+int test(FILE *fp, int x, int right, int down, int divisor) {
+  int trees = 0, row = 0;
+  char buf[200];
+  while (fgets(buf, sizeof(buf), fp) != NULL) {
+    if (row % down == 0) {
+      if (buf[x] == '#') {
+        trees++;
+      }
+      x = (x + right) % divisor;
+    }
+    row++;
+  }
+  fseek(fp, 0, SEEK_SET);
+  return trees;
 }
-		
-int main(int argc, char * argv[]) {
-	
-	if ( argc != 2){
-		printf("Usage : %s [inputfile]\n", argv[0]);
-		exit(-1);
-	}
-	
-	FILE *fp;
-	if ((fp = fopen(argv[1], "r")) == NULL){
-		puts("Error reading file");
-		exit(-1);
-	}
 
-	printf("Tree Count: %d %d %d %d %d Product: %ld\n", test(fp, 0, 1, 1, 31),\
-	test(fp, 0, 3, 1, 31),	test(fp, 0, 5, 1, 31),	test(fp, 0, 7, 1, 31),	test(fp, 0, 1, 2, 31), \
-	((long)test(fp, 0, 1, 1, 31) * test(fp, 0, 3, 1, 31) * test(fp, 0, 5, 1, 31) * test(fp, 0, 7, 1, 31) * test(fp, 0, 1, 2, 31)));
-	
-	fclose(fp);
-	return 0;
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    printf("Usage : %s [inputfile]\n", argv[0]);
+    exit(-1);
+  }
+
+  FILE *fp;
+  if ((fp = fopen(argv[1], "r")) == NULL) {
+    puts("Error reading file");
+    exit(-1);
+  }
+
+  printf("Tree Count: %d %d %d %d %d Product: %ld\n", test(fp, 0, 1, 1, 31),
+         test(fp, 0, 3, 1, 31), test(fp, 0, 5, 1, 31), test(fp, 0, 7, 1, 31),
+         test(fp, 0, 1, 2, 31),
+         ((long)test(fp, 0, 1, 1, 31) * test(fp, 0, 3, 1, 31) *
+          test(fp, 0, 5, 1, 31) * test(fp, 0, 7, 1, 31) *
+          test(fp, 0, 1, 2, 31)));
+
+  fclose(fp);
+  return 0;
 }
